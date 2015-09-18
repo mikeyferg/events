83c215376460438c338c671c1df264311a6b5b5f
Michael F <mikey@mikeys-MacBook-Pro.local>
2015-09-17 21:30:23 -0700

    added actions to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,53 @@
 class UsersController < ApplicationController
+skip_before_filter :verify_authenticity_token
+
+  def index
+    @users = User.all
+    respond_to do |format|
+      format.html
+      format.json { render json: {
+        users: @users
+        }
+       }
+    end
+  end
+  def show
+    @user = find_user
+  end
+  def new
+    @user = User.new
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to events_path
+    else
+      render 'new'
+    end
+  end
+  def edit
+    @user = find_user
+  end
+  def update
+    @user = find_user
+    if @book.update_attributes(user_params)
+      redirect_to user_path(@user)
+    else
+      render 'new'
+    end
+  end
+  def destroy
+    @user = find_user
+    @user.destroy
+    redirect_to root_path
+  end
+
+  private
+  def find_user
+    User.find(params[:id])
+  end
+  def user_params
+    params.require(:user).permit(:name, :pic_url)
+  end
 
 end
