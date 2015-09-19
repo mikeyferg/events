d8f12bb6920e41df74f118ddbe5109399786a249
kastsiushkin <alexkastsiushkin@gmail.com>
2015-09-19 11:52:43 -0700

    Add rack-cors gem to handle cross-origin requests from client

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,13 @@ module EventsApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => %i{delete get options patch post put}
+      end
+    end
+    
   end
 end
