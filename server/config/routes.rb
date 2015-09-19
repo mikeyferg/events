f6d21a0d043831050affc6cf3d87ff81e183b6f5
Michael F <mikey@mikeys-MacBook-Pro.local>
2015-09-18 21:34:55 -0700

    add json to user#show

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'events#index'#, defaults: {format: :json}
+  root 'events#index', defaults: {format: :json}
   match 'auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
   match 'auth/failure', to: redirect('/'), via: [:get, :post]
   match 'signout', to: 'sessions#destroy', as: 'signout', via: [:get, :post]
