a4a6d87ddcb96c56bad52fcdbee3418c296dd0e9
Mike Ferg <mikeyferg@gmail.com>
2015-09-29 20:56:31 -0700

    minor code cleanup

--- server/app/controllers/kimonos_webhooks_controller.rb
+++ server/app/controllers/kimonos_webhooks_controller.rb
@@ -3,7 +3,6 @@ class KimonosWebhooksController < ApplicationController
   #protect_from_forgery :except => :create
   require 'kimono.rb'
 
-#  Kimono.create_events
   def create
     render nothing: true
     Kimono.create_events
