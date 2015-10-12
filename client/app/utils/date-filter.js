caae98be82dddcfc6ce48814f0e189907f5c5323
kastsiushkin <alexkastsiushkin@gmail.com>
2015-10-12 15:25:27 -0700

    Add filters

--- client/app/utils/date-filter.js
+++ client/app/utils/date-filter.js
@@ -3,20 +3,31 @@ import Ember from 'ember';
 const { computed } = Ember;
 
 export function isToday(eventDate) {
-  const today = moment(new Date());
+  const today = moment();
   const event_date = moment(eventDate);
 
   return isSameDay(today, event_date);
 };
 
 export function isTomorrow(eventDate) {
-    const tomorrow = moment(new Date(new Date().getTime() + 24 * 60 * 60 * 1000));
+    const tomorrow = moment().add('days', 1);
     const event_date = moment(eventDate);
 
     return isSameDay(tomorrow, event_date);
 };
 
-// Private functions
-function isSameDay(day1, day2) {
-  return day1.startOf('day').isSame(day2.startOf('day'))
+export function isWeekend(eventDate) {
+    const event_date = moment(eventDate);
+    const friday = moment().day(5);
+    const saturday = moment().day(6);
+    const sunday = moment().day(7);
+
+    return isSameDay(event_date, saturday, sunday);
+};
+
+/* **** Private functions **** */
+function isSameDay(dayToCheck, ...daysArray) {
+    return daysArray.some(function(date) {
+      return dayToCheck.startOf('day').isSame(date.startOf('day'))
+    })
 };
