a1c4ab6b95d725db0d3e12960bee3ae94db088a5
kastsiushkin <alexkastsiushkin@gmail.com>
2015-10-12 19:44:23 -0700

    Add filter all

--- client/app/pods/city/controller.js
+++ client/app/pods/city/controller.js
@@ -2,13 +2,21 @@ import Ember from 'ember';
 import dateFilter from '../../utils/date-filter';
 
 export default Ember.Controller.extend({
-  filter: 'isToday',
+  filter: 'all',
 
   filteredEvents: Ember.computed.filter('model.events', function(event) {
-    return event.get(this.get('filter')) === true;
+    if (this.get('filter') === 'all') {
+      return event;
+    } else {
+      return event.get(this.get('filter')) === true;
+    }
   }).property('model.events', 'model.events.@each.isToday', 'model.events.@each.isTomorrow', 'filter'),
 
   actions: {
+    filterAll() {
+      this.set('filter', 'all');
+    },
+
     filterToday() {
       this.set('filter', 'isToday');
     },
