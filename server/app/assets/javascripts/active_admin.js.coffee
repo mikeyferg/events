c35be20d8353014b6004aff04ef19fb9312413f4
Mike Ferg <mikeyferg@gmail.com>
2015-10-13 20:00:25 -0700

    updated City.find, event-date_only past event fix  and more admin work

--- server/app/assets/javascripts/active_admin.js.coffee
+++ server/app/assets/javascripts/active_admin.js.coffee
@@ -1,1 +1,4 @@
 #= require active_admin/base
+//=require jquery-ui
+
+//=require jquery-ui-timepicker-addon
--- server/app/services/kimono.rb
+++ server/app/services/kimono.rb
@@ -25,36 +25,48 @@ module Kimono
     cost = event['cost'] if event.has_key?('cost')
     source_url = event['source_url']['href'] if event.has_key?('source_url')  && event['source_url'].has_key?('href')
     #start_date = event['start_date'] if event.has_key?('start_date')
-    start_date = Standardizer.date_splitter(event['start_date']) if event.has_key?('start_date')
+    puts event['date_only']
+    date_only = Standardizer.date_splitter(event['date_only']) if event.has_key?('date_only')
     #generic_time = event['start_time'] if event.has_key?('start_time')
-    generic_time = Standardizer.start_time_regex(event['start_time']) if event.has_key?('start_time')
-    start_time = Standardizer.date_time(start_date, generic_time) if event.has_key?('start_time') && event.has_key?('start_date')
+    time_only = Standardizer.start_time_regex(event['time_only']) if event.has_key?('time_only')
+    start_time = Standardizer.date_time(date_only, time_only) if event.has_key?('time_only') && event.has_key?('date_only')
     city_id = 1
-    event = create_update_event(event, name, generic_time, start_time, venue, image_url, summary, address, cost, source_url, start_date, city_id)
-    update_image(event)
-    city = City.find(city_id)
+    event = create_update_event(event, name, time_only, start_time, venue, image_url, summary, address, cost, source_url, date_only, city_id)
+    city = City.find(1)
     city.city_events_will_change!
     city.city_events << event['id']
     city.save
 
   end
 
-  def self.create_update_event(event, name, generic_time, start_time, venue, image_url, summary, address, cost, source_url, start_date, city_id)
+  def self.create_update_event(event, name, time_only, start_time, venue, image_url, summary, address, cost, source_url, date_only, city_id)
     current_event = Event.find_by(name: name)
     if current_event.nil?
-      event = Event.create(name: name, generic_time: generic_time, start_time: start_time, venue:venue, image_url: image_url, summary: summary, address: address, cost:cost, source_url: source_url, start_date: start_date, city_id: city_id)
+      event = Event.create(
+        name: name,
+        time_only: time_only,
+        start_time: start_time,
+        venue: venue,
+        image_url: image_url,
+        summary: summary,
+        address: address,
+        cost: cost,
+        source_url: source_url,
+        date_only: date_only,
+        city_id: city_id
+      )
     else
-      current_event.update(name: name, generic_time: generic_time, start_time: start_time, venue:venue, image_url: image_url, summary: summary, address: address, cost:cost, source_url: source_url, start_date: start_date, city_id: city_id)
+      current_event.update(name: name, time_only: time_only, start_time: start_time, venue: venue, image_url: image_url, summary: summary, address: address, cost:cost, source_url: source_url, date_only: date_only, city_id: city_id)
       current_event
     end
   end
 
-  def self.update_image(event)
-    if !event['image_url'].nil?
-      url = event['image_url']
-      new_image = URI.parse(url)
-      event.update_attribute(:image, new_image)
-    end
-  end
+  # def self.update_image(event)
+  #   if !event['image_url'].nil?
+  #     url = event['image_url']
+  #     new_image = URI.parse(url)
+  #     event.update_attribute(:image, new_image)
+  #   end
+  # end
 
 end
