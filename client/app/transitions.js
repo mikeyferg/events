5180a390af526dbf49d6071828a646eaf05874e6
kastsiushkin <alexkastsiushkin@gmail.com>
2015-10-17 16:05:44 -0700

    Add animation

--- client/app/transitions.js
+++ client/app/transitions.js
@@ -9,10 +9,15 @@
 // }
 
 export default function(){
+  let duration = 500;
   this.transition(
     this.fromRoute('city'),
     this.toRoute('event'),
-    this.use('toLeft'),
-    this.reverse('toRight')
+    this.useAndReverse('explode', {
+      matchBy: 'data-photo-id',
+      use: ['fly-to', {duration}]
+    }, {
+      use: 'fade'
+    })
   );
 }
