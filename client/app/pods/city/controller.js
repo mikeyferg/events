b691becfc47fda0d7b51523375366743962e1f93
kastsiushkin <alexkastsiushkin@gmail.com>
2015-10-29 10:45:46 -0700

    Add pages

--- client/app/pods/city/controller.js
+++ client/app/pods/city/controller.js
@@ -1,18 +1,32 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  filter: 'all',
+  page: 1,
+  cost: null,
+  filter: null,
 
-  filteredEvents: Ember.computed.filter('model.events', function(event) {
-    if (this.get('filter') === 'all') {
-      return event;
+  // filteredEvents: Ember.computed.filter('model.events', function(event) {
+  //   if (this.get('filter') === 'all') {
+  //     return event;
+  //   } else {
+  //     return event.get(this.get('filter')) === true;
+  //   }
+  // }).property('model.events', 'filter'),
+
+  filteredEvents: Ember.computed('filter', 'model', function() {
+    var filter = this.get('filter');
+    var events = this.get('model.events');
+
+    if (filter) {
+      return events.filterBy('filter', filter);
     } else {
-      return event.get(this.get('filter')) === true;
+      return events;
     }
-  }).property('model.events', 'filter'),
+  }),
 
   actions: {
     filterEvents(filterBy) {
+      console.log("rot", this.get('city'));
       this.set('filter', filterBy);
     }
   }
--- client/app/pods/city/route.js
+++ client/app/pods/city/route.js
@@ -1,8 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  queryParams: {
+    page: {
+      refreshModel: true
+    },
+    cost: {
+      refreshModel: true
+    },
+    filter: {
+      refreshModel: true
+    }
+  },
+
   model(params) {
-    return this.store.queryRecord('city', {slug: params.city_slug});
+    return this.store.queryRecord('city', {
+      slug: params.city_slug,
+      page: params.page,
+      cost: params.cost,
+      filter: params.filter
+    });
   },
 
   serialize(city) {
