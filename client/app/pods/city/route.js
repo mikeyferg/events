e321db4a4a25ef70b394c8ab30f8ed1f6e38a45d
kastsiushkin <alexkastsiushkin@gmail.com>
2015-11-04 16:03:04 -0800

    Add meta tags

--- client/app/pods/city/route.js
+++ client/app/pods/city/route.js
@@ -13,8 +13,20 @@ export default Ember.Route.extend({
     };
   },
 
-  afterModel: function(model) {
+  afterModel(model) {
     var cityName = model.get('name');
     $(document).attr('title', `Events in ${cityName}`);
+  },
+
+  headTags() {
+    let model = this.modelFor(this.routeName);
+    return [{
+      type: 'meta',
+      tagId: 'description',
+      attrs: {
+        name: 'description',
+        content: `Events in ${model.get('name')}`
+      }
+    }];
   }
 });
