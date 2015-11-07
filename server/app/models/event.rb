09ada2e64762014459e425e84b7d83db48685ca0
Mike Ferg <mikeyferg@gmail.com>
2015-11-06 20:36:33 -0800

    adding back utc

--- server/app/models/event.rb
+++ server/app/models/event.rb
@@ -50,9 +50,9 @@ class Event < ActiveRecord::Base
   end
 
   def self.by_date_range(date_range = nil)
-    return where('date_only BETWEEN ? AND ?', DateTime.now.beginning_of_day, DateTime.now.end_of_day).all if date_range === 'today'
-    return where('date_only BETWEEN ? AND ?', DateTime.now.tomorrow.beginning_of_day, DateTime.now.tomorrow.end_of_day).all if date_range === 'tomorrow'
-    return where('date_only BETWEEN ? AND ?', DateTime.now.at_beginning_of_week + 4.day, DateTime.now.at_beginning_of_week + 6.day).all if date_range === 'weekend'
+    return where('date_only BETWEEN ? AND ?', DateTime.now.utc.beginning_of_day, DateTime.now.utc.end_of_day).all if date_range === 'today'
+    return where('date_only BETWEEN ? AND ?', DateTime.now.utc.tomorrow.beginning_of_day, DateTime.now.utc.tomorrow.end_of_day).all if date_range === 'tomorrow'
+    return where('date_only BETWEEN ? AND ?', DateTime.now.utc.at_beginning_of_week + 4.day, DateTime.now.utc.at_beginning_of_week + 6.day).all if date_range === 'weekend'
     all
   end
 
