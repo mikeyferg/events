d5e6afd88ee5b14ce89d8efd65023ead65eb0ccd
kastsiushkin <alexkastsiushkin@gmail.com>
2015-11-19 13:46:02 -0800

    Fix schema

--- client/app/pods/application/controller.js
+++ client/app/pods/application/controller.js
@@ -1,9 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  eventsController: Ember.inject.controller('city.events'),
+
   headerTitle: '',
   isMenuEnabled: false,
   isModalVisible: false,
+  search: Ember.computed.reads('eventsController.search'),
 
   menuModifier: Ember.computed('isMenuEnabled', function() {
     if (this.get('isMenuEnabled')) {
--- client/app/pods/city/events/controller.js
+++ client/app/pods/city/events/controller.js
@@ -1,11 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  queryParams: ['page', 'cost', 'date_range', 'free'],
+  queryParams: ['page', 'cost', 'date_range', 'free', 'search'],
   page: 1,
   date_range: 'week',
   cost: null,
   free: false,
+  search: '',
 
   actions: {
     getNextPage() {
