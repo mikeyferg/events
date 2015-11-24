777425af3e8300080d3c472973ea0569fb059e12
kastsiushkin <alexkastsiushkin@gmail.com>
2015-11-24 12:27:28 -0800

    sample

--- client/app/pods/sample/route.js
+++ client/app/pods/sample/route.js
@@ -2,12 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   afterModel() {
-    Ember.$(document).attr('title', 'SP v.1');
+    Ember.$(document).attr('title', 'SP v.2');
   },
 
   setupController(controller, model) {
     this._super(controller, model);
-    this.controllerFor('application').set('headerTitle', 'SP v.1');
+    this.controllerFor('application').set('headerTitle', 'SP v.2');
   },
 
   headTags() {
@@ -16,7 +16,7 @@ export default Ember.Route.extend({
       tagId: 'description',
       attrs: {
         name: 'description',
-        content: 'Sample page version 1, there is nothing else, just a test!'
+        content: 'Sample page version 2, there is nothing else, just a test!'
       }
     }];
   }
