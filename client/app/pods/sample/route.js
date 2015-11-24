82c9f9ef678d4fb3bc74b73955dc1fd7a784e4c5
kastsiushkin <alexkastsiushkin@gmail.com>
2015-11-24 12:14:49 -0800

    Sample test

---
+++ client/app/pods/sample/route.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  afterModel() {
+    Ember.$(document).attr('title', 'SP v.1');
+  },
+
+  setupController(controller, model) {
+    this._super(controller, model);
+    this.controllerFor('application').set('headerTitle', 'SP v.1');
+  },
+
+  headTags() {
+    return [{
+      type: 'meta',
+      tagId: 'description',
+      attrs: {
+        name: 'description',
+        content: 'Sample page version 1, there is nothing else, just a test!'
+      }
+    }];
+  }
+});
