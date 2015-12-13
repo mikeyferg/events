fb03d8726b068e42d0bcf144b7bdc36f1f90706d
Mike Ferg <mikeyferg@gmail.com>
2015-12-13 14:20:22 -0500

    event by_night_only and by_date_range

--- server/app/controllers/events_controller.rb
+++ server/app/controllers/events_controller.rb
@@ -16,12 +16,11 @@ class EventsController < ApplicationController
         .where({ "event_times.start_time": Time.now.utc..6.months.from_now })
         .where({featured: true})
         .distinct
-        .by_category(params[:category])
         .by_date_range(params[:date_range])
-        .by_cost(params[:free], params[:cost])
         .sort_by { rand }
         .take(2)
     else
+
       @events = Event.joins(:event_times)
         .where({ "event_times.start_time": Time.now.utc..6.months.from_now })
         .distinct
