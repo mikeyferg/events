144415ff80213509a067d5c2789ce2bfa60f5b57
kastsiushkin <alexkastsiushkin@gmail.com>
2015-12-13 15:41:31 -0500

    Env

--- client/config/environment.js
+++ client/config/environment.js
@@ -27,7 +27,7 @@ module.exports = function(environment) {
   if (environment === 'development') {
     ENV.APP.LOG_TRANSITIONS = true;
     // ENV.apiHostname = '//localhost:3900',
-    ENV.apiHostname = '//localhost:3900',
+    ENV.apiHostname = '//coyote-api-staging.herokuapp.com',
     ENV.contentSecurityPolicy = {
       'default-src': "none",
       'script-src': "'self' 'unsafe-inline' 'unsafe-eval' https://*.googleapis.com https://*.gstatic.com www.facebook.com connect.facebook.net graph.facebook.com *.google-analytics.com",
